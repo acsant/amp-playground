81f24da704c73e7eb8298ea75ea0e39e2a2e4e44
Akash Sant <acsant@uwaterloo.ca>
2019-07-26 12:19:07 -0700

    Compare status codes but not type

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
 
   app.get('/amp', function(req, res) {
     const responseCode = process.env.STATUS_CODE || 200;
-    if (responseCode === 200) {
+    if (responseCode == 200) {
       res.status(200).sendfile('amp.html', { root: './public' });
     } else {
       res.redirect('/');
