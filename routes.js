e3d886cb065f45c74ef3eb8167afda8c3501a424
Akash Sant <akash.sant@redfin.com>
2019-07-29 14:14:28 -0700

    Dynamic routing + new files

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
   app.get('/akash/amp', function(req, res) {
     const statusCode = process.env.STATUS_CODE || 200;
     if (statusCode == 200) {
-      res.status(200).sendFile('akash_amp.html', { root: 'public' });
+      res.status(200).sendFile('akash_amp.html', { root: './public' });
     } else {
       res.redirect(301, '/akash');
     }
@@ -24,9 +24,9 @@ module.exports = function (app) {
   app.get('/akash', function(req, res) {
     const statusCode = process.env.STATUS_CODE || 200;
     if (statusCode == 200) {
-      res.status(200).sendFile('akash_index.html', { root: 'public' });
+      res.status(200).sendFile('akash_index.html', { root: './public' });
     } else {
-      res.status(200).sendFile('index_unlinked.html', { root: 'public' });
+      res.status(200).sendFile('index_unlinked.html', { root: './public' });
     }
   });
 
