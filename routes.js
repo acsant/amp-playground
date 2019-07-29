d79cde1b19ddcce73321641a5fc8e1a001887998
Akash Sant <akash.sant@redfin.com>
2019-07-29 14:11:53 -0700

    Add a page for me

--- routes.js
+++ routes.js
@@ -9,11 +9,24 @@ module.exports = function (app) {
   });
 
   app.get('/amp', function(req, res) {
-    const responseCode = process.env.STATUS_CODE || 200;
-    if (responseCode == 200) {
-      res.status(200).sendFile('amp.html', { root: './public' });
+    res.status(200).sendFile('amp.html', { root: './public' });
+  });
+
+  app.get('/akash/amp', function(req, res) {
+    const statusCode = process.env.STATUS_CODE || 200;
+    if (statusCode == 200) {
+      res.status(200).sendFile('akash_amp.html', { root: 'public' });
     } else {
-      res.status(301).redirect('/');
+      res.redirect(301, '/akash');
+    }
+  });
+
+  app.get('/akash', function(req, res) {
+    const statusCode = process.env.STATUS_CODE || 200;
+    if (statusCode == 200) {
+      res.status(200).sendFile('akash_index.html', { root: 'public' });
+    } else {
+      res.status(200).sendFile('index_unlinked.html', { root: 'public' });
     }
   });
 
