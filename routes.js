0f4ba6aa4856d479f5622e67370b8397a2b6ca56
Akash Sant <akash.sant@redfin.com>
2019-07-30 16:28:02 -0700

    Add cache update pub key

--- routes.js
+++ routes.js
@@ -1,3 +1,12 @@
+const staticPublicKey = "-----BEGIN PUBLIC KEY-----\n\
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA13zL6i87HX+0p8Cp/ofy\n\
+gRBrNuPDHgU1LLOCnfR/so2kHmph3tlIxo/fowCCLd+Z7NMQrol+3InEdhacf5BO\n\
+jL4H37iCF7eawkP8yS6/t4XVK+D/MaD1gRppM793Sy0dJVhynOL4WN2gWHNVxqeo\n\
+NcZmd7wQMeEKR/PWP/llMgH5/J0+7sMhcy5OraOB9IUl2Ksv43wcWZDv3Olay77r\n\
+Do9MBrTzyYGhlSpcbVcOb9bwXMLrti7z2vt9lYZpPIVRlAhcGbCaxviVXCeod6+d\n\
+g0w9Udi7TYAUp8I7SdkJQ5rj9U8hBDY0uTIGRyNYdOiNNhMA+eULsNjVmNt9Y3lh\n\
+TwIDAQAB\n\
+-----END PUBLIC KEY-----\n";
 module.exports = function (app) {
   app.use(function(req, res, next) {
     res.setHeader('Last-Modified', (new Date()).toUTCString());
@@ -30,6 +39,10 @@ module.exports = function (app) {
     } else {
       res.status(200).sendFile('index_unlinked.html', { root: './public' });
     }
+  });
+
+  app.get('/.well-known/amphtml/apikey.pub', function(req, res) {
+    res.send(staticPublicKey, { 'Content-Type': 'text/plain' }, 200);
   });
 
 }
